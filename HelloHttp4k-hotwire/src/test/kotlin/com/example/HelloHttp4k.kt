f3d828d14b61b09b0f48d2f489fa53579786ea55
David Denton <denton.david@gmail.com>
2021-06-15 16:39:01 +0100

    change port for hotwire

--- HelloHttp4k-hotwire/src/test/kotlin/com/example/HelloHttp4k.kt
+++ HelloHttp4k-hotwire/src/test/kotlin/com/example/HelloHttp4k.kt
@@ -5,5 +5,5 @@ import org.http4k.server.asServer
 
 fun main() {
     val http = FakePokemonApi()
-    val server = Pokemon4k(http).asServer(Undertow(8000)).start()
+    val server = Pokemon4k(http).asServer(Undertow(8001)).start()
 }
