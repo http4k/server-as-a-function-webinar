b6b6853e252a04c3497d670b8259a6b1c77a0bc9
David Denton <denton.david@gmail.com>
2021-10-20 17:39:04 +0100

    upgrade graalvm version

--- HelloHttp4k-serverless-graal/deploy.sh
+++ HelloHttp4k-serverless-graal/deploy.sh
@@ -3,6 +3,6 @@ set -e
 
 ./gradlew clean shadowJar
 
-docker run -v $(pwd):/source http4k/amazonlinux-java-graal-ce-lambda-runtime:amazonlinux2.0.20210326.0-java11-graalce21.1.0 build/libs/HelloHttp4k.jar build/distributions/HelloHttp4k.zip
+docker run -v $(pwd):/source http4k/amazonlinux-java-graal-ce-lambda-runtime:amazonlinux2.0.20211001.0-java11-graalce21.3.0 build/libs/HelloHttp4k.jar build/distributions/HelloHttp4k.zip
 
 pulumi up --yes --stack graalvm
